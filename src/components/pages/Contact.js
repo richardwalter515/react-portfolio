680c240e21420601798d4d80ad994b81172fe74a
Richard Walter <richardwalter515@gmail.com>
2021-02-09 19:01:05 -0500

    updated contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,8 @@ import resume from "../../images/resume-pic.png";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 
 function Contact() {
   return (
@@ -21,21 +22,23 @@ function Contact() {
             <br></br>
             <h5 class="navy cursive-font highlightText">Phone</h5>
             <p>(336) 264-4558</p>
+            <br></br>
           </Card>
-
           <h5 class="navy cursive-font highlightText">Social Media Links</h5>
           <a href="https://www.linkedin.com/in/richard-walter-0bb141121/" target="_blank" class="fa fa-linkedin "></a> 
           <a href="https://twitter.com/richiewalter?lang=en" target="_blank" class="fa fa-twitter"></a>
           <a href="https://github.com/richardwalter515" target="_blank" class="fa fa-github"></a> 
+
         </Col>
       <Col>
-        <Card className="bg-dark text-white">
-          <Card.Img src={resume} alt="Card image" href="./assets/Resume-coding.pdf" download="Resume" />
-           <Card.ImgOverlay>
-              <Card.Title>Click to Download My Resume</Card.Title>
-          </Card.ImgOverlay>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant="top" src={resume} />
+          <Card.Body>
+            <Card.Title>My Resume</Card.Title>
+            <a href="./assets/Resume-coding.pdf" download="Resume" class="btn btn-outline-primary">Download PDF</a>
+          </Card.Body>
         </Card>
-        <a href="./assets/Resume-coding.pdf" download="Resume" class="btn btn-outline-primary">PDF</a>
+        
       </Col>
     </Row>
   </Container>
