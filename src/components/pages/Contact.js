b58e8fd2449f8c352b4ccfbbc560d58e04a0ab8e
Richard Walter <richardwalter515@gmail.com>
2025-08-15 13:46:49 -0400

    updated resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from "../../images/resumePic.png";
+import resume from "../../images/resumePic2.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
